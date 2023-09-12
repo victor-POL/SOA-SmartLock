ec7202d00ec94b6ab3c81a8c8d7d9b2112831e20
Victor <99147005+victor-POL@users.noreply.github.com>
2023-09-12 09:17:14 -0300

    Add event puerta cerra and remove event persona se fue

--- src/Events.h
+++ src/Events.h
@@ -2,12 +2,12 @@
 #define EVENTO_CONTINUE                             1000
 #define EVENTO_PERSONA_DETECTADA                    1001
 #define EVENTO_PERSONA_NO_DETECTADA                 1002
-#define EVENTO_PERSONA_SE_FUE                       1003
 #define EVENTO_VALIDAR_CLAVE                        1004
 #define EVENTO_CLEAR_CLAVE_INGRESADA                1005
 #define EVENTO_CARACTER_INGRESADO                   1006
-#define EVENTO_TIMEOUT                              1007
+#define EVENTO_TIMEOUT_VALIDACION_CLAVE             1007
 #define EVENTO_CLAVE_VALIDA                         1008
 #define EVENTO_CLAVE_INVALIDA                       1009
 #define TIMEOUT_APERTURA_PUERTA                     1010
 #define EVENTO_PUERTA_ABIERTA                       1011
+#define EVENTO_PUERTA_CERRADA                       1012
